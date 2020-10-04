fc85f84057cc1f98f055089972f885b2e7cd9d17
fexd12 <felipe.demelo@hotmail.com>
2020-10-04 15:49:16 -0300

    teste email

--- backend/worker.py
+++ backend/worker.py
@@ -1,7 +1,7 @@
 
 import redis
 from rq import Connection,Worker
-redis_url = 'redis://34.95.161.20:6379'
+redis_url = ''
 redis_conn = redis.from_url(redis_url)
 with Connection(redis_conn):
         worker = Worker('default')
