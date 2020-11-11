60777567b29edc23da3bb01db404584d82a660ee
fexd12 <felipe.demelo@hotmail.com>
2020-11-11 11:48:20 -0300

    adicionado host a config

--- config.py
+++ config.py
@@ -17,3 +17,4 @@ class Config(object):
     REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
     REDIS_PORT = os.environ.get('REDIS_PORT')
     REDIS_HOST = os.environ.get('REDIS_HOST')
+    HOST = os.environ.get('HOST')
